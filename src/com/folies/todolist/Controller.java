7a39647631d3604fb24cd252fcd7d54e39ca89fd
Andrey Ponamarchuk <ponamarchuk.andrey@icloud.com>
2020-03-19 23:27:55 +0500

    add DialogPane

--- src/com/folies/todolist/Controller.java
+++ src/com/folies/todolist/Controller.java
@@ -3,14 +3,18 @@ package com.folies.todolist;
 import com.folies.todolist.datamodel.ToDoData;
 import com.folies.todolist.datamodel.ToDoItem;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.control.SelectionMode;
-import javafx.scene.control.TextArea;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
 
+import java.io.IOException;
 import java.time.format.DateTimeFormatter;
 
 public class Controller {
+
+    @FXML
+    private BorderPane mainBorderPain;
 
     @FXML
     private ListView<ToDoItem> toDoListView;
@@ -34,5 +38,18 @@ public class Controller {
         toDoListView.getItems().setAll(ToDoData.getInstance().getToDoItems());
         toDoListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
         toDoListView.getSelectionModel().selectFirst();
+    }
+
+    @FXML
+    private void showNewItemDialog() {
+        Dialog<ButtonType> dialog = new Dialog<>();
+        dialog.initOwner(mainBorderPain.getScene().getWindow());
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("toDoItemDialog.fxml"));
+            dialog.getDialogPane().setContent(root);
+        } catch (IOException e) {
+            System.err.println("Couldn't load the dialog");
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/com/folies/todolist/DialogController.java
@@ -0,0 +1,4 @@
+package com.folies.todolist;
+
+public class DialogController {
+}
